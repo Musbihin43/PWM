d0b75d18e0c48705833362fb60859059625d28e9
musbihin43 <imchamelon@gmail.com>
2021-06-29 10:39:23 +0700

    second commit

--- js/image.main.js
+++ js/image.main.js
@@ -4,9 +4,11 @@ const dog_btn = document.getElementById('dog_btn');
 const cat_result = document.getElementById('cat_result');
 const dog_result = document.getElementById('dog_result');
 
+//Menambahkan event listener untuk memanggil function saat tombol di klik
 cat_btn.addEventListener('click', getRandomCat);
 dog_btn.addEventListener('click', getRandomDog);
 
+//Mengambil gambar dari API untuk kucing  
 function getRandomCat() {
     fetch('https://aws.random.cat/meow')
         .then(res => res.json())
@@ -14,7 +16,7 @@ function getRandomCat() {
             cat_result.innerHTML = `<img src=${data.file} alt="cat" />`
         });
 }
-
+//Mengambil gambar dari API untuk anjing  
 function getRandomDog() {
     fetch('https://random.dog/woof.json')
         .then(res => res.json())
